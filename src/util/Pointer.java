4bb9c5d5933d13dba9fcd7ef62f9f551cdee4894
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2017-05-28 15:59:21 -0400

    Added Comments to Pointer Class

--- src/util/Pointer.java
+++ src/util/Pointer.java
@@ -12,27 +12,47 @@ import java.nio.IntBuffer;
 public class Pointer {
 
 	/**
-	 * Buffer to hold
+	 * Buffer to hold the integer value.
 	 */
 	private IntBuffer buffer;
 
+	/**
+	 * Constructs a Pointer object and initializes the buffer.
+	 */
 	public Pointer() {
 		this.buffer = IntBuffer.allocate(1);
 	}
-
+	
+	/**
+	 * Returns the value at this Pointer.
+	 * 
+	 * @return The value at this Pointer.
+	 */
 	public int get() {
 		return (int) buffer.get(0);
 	}
 
+	/**
+	 * Returns the IntBuffer associated with this Pointer.
+	 * 
+	 * @return The IntBuffer associated with this Pointer.
+	 */
 	public IntBuffer getBuffer() {
 		return buffer;
 	}
 	
+	/**
+	 * Sets the value at this Pointer to the given value.
+	 * 
+	 * @param newValue The new value at this Pointer.
+	 */
 	public void set(int newValue) {
 		buffer.put(newValue, 0);
 	}
 
-	@Override
+	/**
+	 * Returns a String representation of this Pointer.
+	 */
 	public String toString() {
 		return String.valueOf(buffer.get(0));
 	}
