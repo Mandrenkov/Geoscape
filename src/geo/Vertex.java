3478c3a1c3b5f7222bb91f3fb160468c8f957fdd
Mandrenkov <M.Andrenkov@gmail.com>
2018-02-18 11:53:25 -0500

    Refactored the Vertex class.

--- src/geo/Vertex.java
+++ src/geo/Vertex.java
@@ -5,16 +5,14 @@ package geo;
  * @since February 15, 2018
  * @version 1.1
  *
- * <pThe <b>Vertex</b> class represents a 3D Vertex.</p>
+ * <pThe <b>Vertex</b> class represents a point in R3.</p>
  */
 public class Vertex {
-	/**
-	 * The dimensional components of this Vertex relative to the origin.
-	 */
-	private float x, y, z;
+	// Public members
+	// -------------------------------------------------------------------------
 
 	/**
-	 * Constructs a Vertex object with the given 3D coordinate.
+	 * Constructs a Vertex representing the given 3D coordinate.
 	 *
 	 * @param x The X-coordinate of this Vertex.
 	 * @param y The Y-coordinate of this Vertex.
@@ -27,39 +25,44 @@ public class Vertex {
 	}
 
 	/**
-	 * Returns a new Vertex at the midpoint between this Vertex and the given Vertex.
+	 * Constructs a Vertex at the midpoint of the given Vertices.
 	 *
-	 * @param Vertex The second Vertex.
-	 * 
-	 * @return The midpoint Vertex.
+	 * @param v1 The first Vertex.
+	 * @param v2 The second Vertex.
 	 */
-	public Vertex average(Vertex Vertex) {
-		return new Vertex((this.x + Vertex.x)/2, (this.y + Vertex.y)/2, (this.z + Vertex.z)/2);
+	public Vertex(Vertex v1, Vertex v2) {
+		this.x = (v1.x + v2.x)/2;
+		this.y = (v1.y + v2.y)/2;
+		this.z = (v1.z + v2.z)/2;
 	}
 
 	/**
-	 * Changes the elevation (Z) of this Vertex by a random value within the given range.
+	 * Changes the elevation of this Vertex by a random value within the given range.
 	 *
-	 * @param range The maximum magnitude of the elevation change.
+	 * @param magnitude The maximum magnitude of the change in elevation.
 	 */
-	public void bump(float range) {
-		z += (float) (range*(2*Math.random()- 1));
+	public void bump(float magnitude) {
+		float random = (float) (Math.random() - 0.5);
+		this.z += magnitude*2*random;
 	}
 
 	/**
 	 * Returns the distance between this Vertex and the given Vertex.
 	 *
-	 * @param Vertex The second Vertex.
-	 * @return The distance between the Vertexs.
+	 * @param vertex The other Vertex.
+	 * 
+	 * @return The distance between the Vertices.
 	 */
-	public float distance(Vertex Vertex) {
-		return (float) Math.sqrt(Math.pow(this.x - Vertex.x, 2) + Math.pow(this.y - Vertex.y, 2));
+	public float distance(Vertex vertex) {
+		return (float) Math.sqrt(Math.pow(this.x - vertex.x, 2)
+							   + Math.pow(this.y - vertex.y, 2)
+							   + Math.pow(this.z - vertex.z, 2));
 	}
 
 	/**
 	 * Returns the X-coordinate of this Vertex.
 	 *
-	 * @return The X-coordinate of this Vertex.
+	 * @return The X-coordinate.
 	 */
 	public float getX() {
 		return x;
@@ -68,7 +71,7 @@ public class Vertex {
 	/**
 	 * Returns the Y-coordinate of this Vertex.
 	 *
-	 * @return The Y-coordinate of this Vertex.
+	 * @return The Y-coordinate.
 	 */
 	public float getY() {
 		return y;
@@ -77,7 +80,7 @@ public class Vertex {
 	/**
 	 * Returns the Z-coordinate of this Vertex.
 	 *
-	 * @return The Z-coordinate of this Vertex.
+	 * @return The Z-coordinate.
 	 */
 	public float getZ() {
 		return z;
@@ -86,7 +89,7 @@ public class Vertex {
 	/**
 	 * Sets the X-coordinate of this Vertex to the specified value.
 	 *
-	 * @param x The new X-coordinate
+	 * @param x The new X-coordinate.
 	 */
 	public void setX(float x) {
 		this.x = x;
@@ -95,7 +98,7 @@ public class Vertex {
 	/**
 	 * Sets the Y-coordinate of this Vertex to the specified value.
 	 *
-	 * @param y The new Y-coordinate
+	 * @param y The new Y-coordinate.
 	 */
 	public void setY(float y) {
 		this.y = y;
@@ -104,16 +107,61 @@ public class Vertex {
 	/**
 	 * Sets the Z-coordinate of this Vertex to the specified value.
 	 *
-	 * @param z The new Z-coordinate
+	 * @param z The new Z-coordinate.
 	 */
 	public void setZ(float z) {
 		this.z = z;
 	}
 
 	/**
+	 * Scales the coordinates of this Vertex by the given scaling factor.
+	 * 
+	 * @param scalar The scaling factor.
+	 */
+	public void scale(float scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+		this.z *= scalar;
+	}
+
+	/**
+	 * Translates this Vertex by the given coordinate values.
+	 * 
+	 * @param dx The amount to translate the X-coordinate of this Vertex.
+	 * @param dy The amount to translate the Y-coordinate of this Vertex.
+	 * @param dz The amount to translate the Z-coordinate of this Vertex.
+	 */
+	public void translate(float dx, float dy, float dz) {
+		this.x += dx;
+		this.y += dy;
+		this.z += dz;
+	}
+
+	/**
 	 * Returns a String representation of this Vertex.
+	 * 
+	 * @return The String representation.
 	 */
 	public String toString() {
 		return String.format("Vertex: (%.2f, %.2f, %.2f)", x, y, z);
 	}
+
+
+	// Private members
+	// -------------------------------------------------------------------------
+
+	/**
+	 * The X-coordinate of this Vertex.
+	 */
+	private float x;
+	
+	/**
+	 * The Y-coordinate of this Vertex.
+	 */
+	private float y;
+	
+	/**
+	 * The Z-coordinate of this Vertex.
+	 */
+	private float z;
 }
