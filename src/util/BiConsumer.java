512013db1037e21b3388443d6497ace5b3a0a672
Mandrenkov <M.Andrenkov@gmail.com>
2018-07-02 17:59:27 -0400

    Added a BiConsumer class which represents a Consumer with two generic parameters.

---
+++ src/util/BiConsumer.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.util.Objects;
+
+/**
+ * The BiConsumer interface is an adaption of the java.util.function.Consumer
+ * interface that accepts two parameters.
+ */
+@FunctionalInterface
+interface BiConsumer<T, U> {
+    /**
+     * @see This function carries the same semantic spirit as {@link java.util.function.Consumer#accept(Object)}.
+     */
+    void accept(T t, U u);
+
+    /**
+     * @see This function carries the same semantic spirit as {@link java.util.function.Consumer#andThen(java.util.function.Consumer)}.
+     */
+    default BiConsumer<T, U> andThen(BiConsumer<? super T, ? super U> after) {
+        Objects.requireNonNull(after);
+        return (T t, U u) -> {
+            this.accept(t, u);
+            after.accept(t, u);
+        };
+    }
+}
